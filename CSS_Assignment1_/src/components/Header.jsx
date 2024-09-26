8067da26e5c87fd25ed095228600ec09b7f80a68
7vTr0x7 <7vTrox7@gmail.com>
2024-09-26 12:24:06 +0530

    Header

--- CSS_Assignment1_/src/components/Header.jsx
+++ CSS_Assignment1_/src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./header.style.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg">
+      <nav className="navbar">
+        <ul className="nav">
+          <li className="nav-items">HOME</li>
+          <li className="nav-items">TAG</li>
+          <li className="nav-items">AUTHOR</li>
+          <li className="nav-items">HELP</li>
+        </ul>
+        <div></div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
