5f5f731c36659e9c20dba3bf881c050efbcf26aa
7vTr0x7 <7vTrox7@gmail.com>
2024-09-26 13:37:44 +0530

    MainSection

--- CSS_Assignment1_/src/App.jsx
+++ CSS_Assignment1_/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
+import MainSection from "./components/MainSection";
 import Top from "./components/Top";
 
 function App() {
   return (
     <>
       <Header />
-      <Top/>
+      <Top />
+      <MainSection />
     </>
   );
 }
---
+++ CSS_Assignment1_/src/components/MainSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./main.style.css";
+
+const MainSection = () => {
+  return (
+    <main>
+      <div className="row">
+        <div>
+          <img
+            className="main-card-img"
+            alt="main-card"
+            src="https://res.cloudinary.com/dwxq0majx/image/upload/fl_preserve_transparency/v1727264798/ghostImage1_gsl1i7.jpg"
+          />
+        </div>
+        <div className="col">
+          <div>
+            <h2>GETTING STARTED</h2>
+            <p className="title">Welcome to Ghost</p>
+          </div>
+          <p className="para">
+            Welcome, it's great to have you here. We know that first impressions
+            are important, so we've populated your new site with some initial
+            getting started posts that will help you get familiar with
+            everything in no time.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MainSection;
